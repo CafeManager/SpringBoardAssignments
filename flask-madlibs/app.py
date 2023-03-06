e40d58a2a7da37759b2a8d2593a155ccfb2fe890
cafemanager <cafemanager97@gmail.com>
2023-03-05 20:43:05 -0800

    added flask madlibs solution

--- flask-madlibs/app.py
+++ flask-madlibs/app.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, request 
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "hi"
 
-@app.route('/')
-def home_page():
-    return 'hi'
+debug = DebugToolbarExtension(app)
+@app.route('/form')
+def home_page():    
+    return render_template('form.html', prompts=story.prompts)
+
+@app.route('/generate')
+def generate():
+    generated = {}
+    for x in request.args:
+        generated[x] = request.args[x]
+    generatedStory = story.generate(generated)
+    return render_template('story.html', story = generatedStory)
