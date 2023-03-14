5d59dc9162644e4b250d9bde6822e3a7db6a4e5a
cafemanager <cafemanager97@gmail.com>
2023-03-13 22:13:57 -0700

    step 2 of assignment completed. Start at step 3.

--- flask-boggle/app.py
+++ flask-boggle/app.py
@@ -1,3 +1,21 @@
 from boggle import Boggle
+from flask import Flask, render_template, session
+from flask_debugtoolbar import DebugToolbarExtension
 
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "SecRETKey"
 boggle_game = Boggle()
+debug = DebugToolbarExtension(app)
+
+def initialize_board():
+    if not session.get("board"):
+        board = boggle_game.make_board()
+        session["board"] = board
+
+@app.route("/home")
+def home_page():
+    initialize_board()
+    return(render_template("home.html", board=session["board"]))
+
+def main():
+    initialize_board()
