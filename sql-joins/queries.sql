df96dea2efd93837e1b6c12f806f67d24a7cb0a1
cafemanager <cafemanager97@gmail.com>
2023-03-29 22:34:47 -0700

    add sql join assignment

--- sql-joins/queries.sql
+++ sql-joins/queries.sql
@@ -1,1 +1,4 @@
 -- write your queries here
+SELECT * FROM owners LEFT JOIN vehicles ON vehicles.owner_id = owners.id;
+SELECT o.first_name, o.last_name, count(*) as count FROM owners o JOIN vehicles v ON v.owner_id = o.id GROUP BY o.first_name, o.last_name ORDER BY first_name ASC;
+SELECT o.first_name, o.last_name, avg(price) as average_price, count(*) as count FROM owners o JOIN vehicles v ON v.owner_id = o.id GROUP BY o.first_name, o.last_name HAVING avg(price) > 10000 ORDER BY first_name DESC;
